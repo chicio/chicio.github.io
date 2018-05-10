07a51ad3c90b27b6d0dd859ef74af19bbec3deab
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2018-05-10 22:41:32 +0200

    Blog improved font load :rocket:

--- _js/blog.js
+++ _js/blog.js
@@ -23,32 +23,30 @@ function isAMobileDevice() {
 
 function disableScroll() {
     if (isAMobileDevice()) {
-        $("html").css('overflow-y','hidden');
+        $("html").css('overflow-y', 'hidden');
         $("body").css('position', 'fixed');
     }
 }
 
 function showBlog() {
     $(document).ready(function () {
-        $(window).on("load", function() {
-            var showBlogTimeline = new TimelineMax({delay: 0.2});
-            showBlogTimeline.to("#loader", 0.2, {opacity: 0});
-            showBlogTimeline.to("#loading-screen", 0.4, {
-                xPercent: -100, onComplete: function () {
-                    enableScroll()
-                }
-            }, "+=0.4");
-        });
+        var showBlogTimeline = new TimelineMax({delay: 0.2});
+        showBlogTimeline.to("#loader", 0.2, {opacity: 0});
+        showBlogTimeline.to("#loading-screen", 0.4, {
+            xPercent: -100, onComplete: function () {
+                enableScroll()
+            }
+        }, "+=0.4");
     });
 }
 
 function enableScroll() {
-    $("html").css('overflow-y','auto');
+    $("html").css('overflow-y', 'auto');
     $("body").css('position', '');
 }
 
 function cookieConsent() {
-    window.addEventListener("load", function(){
+    window.addEventListener("load", function () {
         window.cookieconsent.initialise({
             "palette": {
                 "popup": {
@@ -63,26 +61,27 @@ function cookieConsent() {
             "theme": "classic",
             "content": {
                 "dismiss": "Ok",
-                "href": window.location.protocol + "//" +  window.location.host + "/cookie-policy.html",
+                "href": window.location.protocol + "//" + window.location.host + "/cookie-policy.html",
                 "message": "This website uses cookies to ensure you get the best experience."
             }
-        })});
+        })
+    });
 }
 
 function runFuckAdBlock() {
-    if(typeof fuckAdBlock !== 'undefined' || typeof FuckAdBlock !== 'undefined') {
+    if (typeof fuckAdBlock !== 'undefined' || typeof FuckAdBlock !== 'undefined') {
         $('#adblock-alert').show();
     } else {
         var importFAB = document.createElement('script');
-        importFAB.onload = function() {
-            fuckAdBlock.onDetected(function() {
+        importFAB.onload = function () {
+            fuckAdBlock.onDetected(function () {
                 $('#adblock-alert').show();
             });
-            fuckAdBlock.onNotDetected(function() {
+            fuckAdBlock.onNotDetected(function () {
                 $('#adblock-alert').hide();
             });
         };
-        importFAB.onerror = function() {
+        importFAB.onerror = function () {
             $('#adblock-alert').show();
         };
         importFAB.integrity = 'sha256-xjwKUY/NgkPjZZBOtOxRYtK20GaqTwUCf7WYCJ1z69w=';
