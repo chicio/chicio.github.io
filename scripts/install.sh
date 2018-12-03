2488ff9fbedc244a594df6f935fe8c7f75e68edb
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2018-12-03 22:08:09 +0100

    Added remove of docker images if they exists

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,10 @@ git submodule update --init
 # Install/Update core utils to use docker startup github-pages function
 brew install coreutils
 
+# Clean old docker images
+docker images -a | grep "gh-pages" | awk '{print $3}' | xargs docker rmi
+docker images -a | grep "ruby" | awk '{print $3}' | xargs docker rmi
+
 # Create github pages docker image
 cd pages-gem
 make image
