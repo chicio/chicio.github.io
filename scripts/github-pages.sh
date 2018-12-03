59e4f7b4ab330e79efbafb0a43a794c02a6be702
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2018-12-03 15:19:11 +0100

    Server invoked correctly

--- scripts/github-pages.sh
+++ scripts/github-pages.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
-# Load github pages function 
-source pages-gem/contrib/func.sh
-
 # Run docker image using github pages function
-github-pages
+cd pages-gem
+expose SITE=../
+make server
