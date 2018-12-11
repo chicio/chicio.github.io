6c62b0c19702a431d16546ffd91bbbe2dded121b
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2018-12-11 22:40:34 +0100

    Added service worker first raw setup :rocket:

--- sw.js
+++ sw.js
@@ -1,24 +1,16 @@
+---
+---
 importScripts('/cache-polyfill.js');
 
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('fabrizioduroni_v2.0.0').then(function(cache) {
-     return cache.addAll([
-       '/',
-       '/?utm_source=pwa',
-       '/assets/images/fabrizio-duroni.jpg',
-       '/assets/images/xcode.png'
-     ]);
-   })
- );
-});
-
-// Network falling back to the cache
 self.addEventListener('fetch', function(event) {
-  console.log(event.request.url);
   event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request);
+    caches.open('fabrizioduroni.it_2.0.0').then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
     })
   );
 });
