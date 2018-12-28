17babd3b739a60546c201b268c67ba00d8317f72
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2018-12-28 20:55:19 +0100

    Fixed other urls :rocket:

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,4 +7,4 @@ npm run build
 npm run generate
 
 # Unit test website
-htmlproofer --assume-extension --http-status-ignore "999" ./_site
+htmlproofer --check-favicon --assume-extension --http-status-ignore "999" ./_site
