e4254d3f3376f8179b2f2f16f64b8cb86c3ed035
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2019-01-02 21:24:23 +0100

    Fixed script for version :rocket:

--- _scripts/version.sh
+++ _scripts/version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Update include file of packgae.json version
-echo $npm_package_version > _includes/version.txt
+echo -n $npm_package_version > _includes/version.txt
 
 # Add all files modified (not only package.json)
 git add .
