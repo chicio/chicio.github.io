e3199d9abc9e92a549af6564aef762e882e331b7
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2019-01-02 21:30:05 +0100

    Changed echo with println :rocket:

--- _scripts/version.sh
+++ _scripts/version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Update include file of packgae.json version
-echo -n $npm_package_version > _includes/version.txt
+printf $npm_package_version > _includes/version.txt
 
 # Add all files modified (not only package.json)
 git add .
