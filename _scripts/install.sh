d0a9f4704a8ed7c324d31a6993d756e7741bb08c
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2019-03-10 15:50:30 +0100

    Fixed workflow to not use sudo by using rvm :rocket:

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -4,6 +4,11 @@
 npm install --global gulp-cli@2.0.1
 npm install --global flow-typed@2.5.1
 
+# Install rvm
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable --ruby
+source /Users/chicio/.rvm/scripts/rvm
+
 # Install gem dependecies
-sudo gem install bundler -n /usr/local/bin
+sudo gem install bundler
 bundle install --path vendor/bundle
