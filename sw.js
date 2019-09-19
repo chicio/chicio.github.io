da566b2d40739028d68348c9d4533235495926f9
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2019-09-19 13:45:21 +0200

    First implementation of offline page :rocket:

--- sw.js
+++ sw.js
@@ -1,10 +1,14 @@
 ---
 ---
-importScripts('/cache-polyfill.js');
+importScripts('/cache-polyfill.js')
 
-const siteCacheName = 'chicioCodingCache{% include version.txt %}';
+const siteCacheName = 'chicioCodingCache{% include version.txt %}'
+const offlineUrl = '/offline.html'
+const offlineImageUrl = '/assets/images/no-wifi.png'
 const dependenciesUrls = [
   "/favicon.ico",
+  offlineUrl,
+  offlineImageUrl,
   {% include service-worker-css-home-urls.js %}
   {% include service-worker-css-blog-archive-urls.js %}
   {% include service-worker-css-blog-home-urls.js %}
@@ -14,69 +18,75 @@ const dependenciesUrls = [
   {% include service-worker-css-cookie-policy-urls.js %}
   {% include service-worker-js-home-urls.js %}
   {% include service-worker-js-blog-urls.js %}
-];
+]
 
 self.addEventListener('install', (event) => {
-  self.skipWaiting();
+  self.skipWaiting()
   event.waitUntil(
     caches.open(siteCacheName).then((cache) => {
-      return cache.addAll(dependenciesUrls);
+      return cache.addAll(dependenciesUrls)
     })
-  );
-});
+  )
+})
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+  event.waitUntil(clients.claim())
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.filter((cacheName) => {
           return cacheName !== siteCacheName
         }).map((cacheName) => {
-          return caches.delete(cacheName);
+          return caches.delete(cacheName)
         })
-      );
+      )
     })
-  );
-});
+  )
+})
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
       caches.open(siteCacheName).then(async (cache) => {
         return cache.match(event.request).then((response) => {
-          return response || fetch(event.request).then((response) => {
-            cache.put(event.request, response.clone());
-            return response;
-          });
-        });
+          return response || fetch(event.request)
+            .then((response) => {
+              cache.put(event.request, response.clone())
+              return response
+            }).catch((error) => {
+              if (event.request.mode === 'navigate' ||  
+                  (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))
+              ) {
+                return caches.match(offlineUrl)
+              }
+            })
+        })
       })
-    );  
-});
+    )  
+})
 
 self.addEventListener('message', (event) => {
-  const isARefresh = (event) => event.data.message === 'refresh';
+  const isARefresh = (event) => event.data.message === 'refresh'
 
   const createDeleteOperationFor = (url, siteCache, requests) => siteCache
     .delete(requests
-    .find((request) => request.url === url));
+    .find((request) => request.url === url))
 
   const createDeleteOperationsForImages = (siteCache, requests) => requests
     .filter((request) => request.url.endsWith('.jpg') && request.url.includes('posts'))
     .map((request) => siteCache.delete(request))
 
-  const sendRefreshCompletedMessageToClient = (event) => event.ports[0].postMessage({refreshCompleted: true});
+  const sendRefreshCompletedMessageToClient = (event) => event.ports[0].postMessage({refreshCompleted: true})
 
   if (isARefresh(event)) {
-    const refreshMessage = {shouldRefresh: true};
+    const refreshMessage = {shouldRefresh: true}
     caches.open(siteCacheName).then((siteCache) => {
       siteCache.keys().then((requests) => {
         const deleteRequestToBeRefreshed = createDeleteOperationFor(event.data.url, siteCache, requests)
         const deleteRequestsForImagesToBeRefreshed = createDeleteOperationsForImages(siteCache, requests)
         Promise.all([deleteRequestToBeRefreshed, ...deleteRequestsForImagesToBeRefreshed])
           .then(() => sendRefreshCompletedMessageToClient(event))
-          .catch(() => sendRefreshCompletedMessageToClient(event));
-      });
-    });  
+          .catch(() => sendRefreshCompletedMessageToClient(event))
+      })
+    }) 
   }
-});
-
+})
