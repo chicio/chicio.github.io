c456c6b51e12ed7d51839db48a459f9dbbd7714c
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2019-11-04 22:38:47 +0100

    Force zsh as shell bash env :rocket:

--- _scripts/setup.sh
+++ _scripts/setup.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env zsh
 
 # Install gulp cli and flow typed
-npm install --global gulp-cli@2.0.1
-npm install --global flow-typed@2.5.1
+npm install --global gulp-cli@2.2.0
+npm install --global flow-typed@2.6.2
 
-# Set rvm to use ruby 2.6.0
+# Set rvm to use ruby 2.6.0 (load rvm through source file)
+source ~/.rvm/scripts/rvm
 rvm install 2.6.0
 rvm use 2.6.0
 
