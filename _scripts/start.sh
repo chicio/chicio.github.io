75eeca7420c843ab9c56a98d9c14763284e81918
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2019-11-16 23:48:33 +0100

    Improving lighthouse ci testing :rocket:

--- _scripts/start.sh
+++ _scripts/start.sh
@@ -7,5 +7,10 @@ rvm use 2.6.0
 # Clean previous build
 rm -Rf _site/
 
-# Run pages-gem with bundler
-bundle exec jekyll serve --host=0.0.0.0
+# Run pages-gem with bundler.
+# Add the parameter
+# --host=0.0.0.0
+# to be able to connect to the jekyll local instance from other device on the
+# same network (e.g. your smartphone) 
+# See https://stackoverflow.com/questions/16608466/connect-to-a-locally-built-jekyll-server-using-mobile-devices-in-the-lan
+bundle exec jekyll serve 
