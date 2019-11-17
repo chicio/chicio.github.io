ebf65411f2cd7d2ad21f9a035f84b106e2940679
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2019-11-17 01:53:23 +0100

    Final test because I don't understand why some checks still don't work :rocket:

--- _scripts/setup.sh
+++ _scripts/setup.sh
@@ -5,7 +5,6 @@ npm install --global n@6.1.2
 npm install --global gulp-cli@2.2.0
 npm install --global flow-typed@2.6.2
 npm install --global flowgen@1.10.0  
-npm install --global @lhci/cli@0.3.x
 
 # Set rvm to use ruby 2.6.0 (load rvm through source file)
 source ~/.rvm/scripts/rvm
