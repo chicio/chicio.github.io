a648139a2cbf378f7908a3ac0986190e97a8b53e
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2019-11-20 07:50:01 +0100

    Keep lighthouse on after run (too much) unstable :rocket:

--- _scripts/test.sh
+++ _scripts/test.sh
@@ -16,7 +16,4 @@ bundle exec htmlproofer --check-html \
                         --url-ignore "https://www.volagratis.com/,https://www.rumbo.es/,http://www.ivl.disco.unimib.it,http://www.power-sure.com/lumens.htm,http://fuckingclosuresyntax.com,http://devernay.free.fr/cours/opengl/materials.html,http://id3.org/,http://id3.org/d3v2.3.0,http://reactivex.io/,http://codingdojo.org/WhatIsCodingDojo/,http://xip.io/,http://codingdojo.org/kata/Minesweeper/"  \
                         --http-status-ignore "999" \
                         ./_site
-
-#lighthouse performance test
-lhci autorun
                                                 
