ad26400bbbe5643a42b0637f5d368e8d0abc1166
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2020-04-20 14:16:31 +0200

    Try to fix tests :rocket:

--- _scripts/test.sh
+++ _scripts/test.sh
@@ -11,5 +11,6 @@ bundle exec htmlproofer --check-html \
                         --assume-extension \
                         --url-ignore "https://www.volagratis.com/,https://www.rumbo.es/,http://www.ivl.disco.unimib.it,http://www.power-sure.com/lumens.htm,http://fuckingclosuresyntax.com,http://devernay.free.fr/cours/opengl/materials.html,http://id3.org/,http://id3.org/d3v2.3.0,http://reactivex.io/,http://codingdojo.org/WhatIsCodingDojo/,http://xip.io/,http://codingdojo.org/kata/Minesweeper/,http://id3.org/Implementations"  \
                         --http-status-ignore "999" \
+                        --typhoeus_config "{\"followlocation\": true}"
                         ./_site
                                                 
