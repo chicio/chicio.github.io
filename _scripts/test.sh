62d4f575937b2b7bd606bb57ef34be4cd609177e
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2020-05-12 21:13:04 +0200

    Added url to ignore in tests :rocket:

--- _scripts/test.sh
+++ _scripts/test.sh
@@ -9,7 +9,7 @@ bundle exec htmlproofer --check-html \
                         --check-opengraph \
                         --enforce-https \
                         --assume-extension \
-                        --url-ignore "https://www.volagratis.com/,https://www.rumbo.es/,http://www.ivl.disco.unimib.it,http://www.power-sure.com/lumens.htm,http://fuckingclosuresyntax.com,http://devernay.free.fr/cours/opengl/materials.html,http://id3.org/,http://id3.org/d3v2.3.0,http://reactivex.io/,http://codingdojo.org/WhatIsCodingDojo/,http://xip.io/,http://codingdojo.org/kata/Minesweeper/,http://id3.org/Implementations,https://developer.android.com/studio/intro/keyboard-shortcuts.html,https://developer.android.com/studio/publish/app-signing#generate-key,https://developer.android.com/studio/publish/upload-bundle"  \
+                        --url-ignore "https://www.volagratis.com/,https://www.rumbo.es/,http://www.ivl.disco.unimib.it,http://www.power-sure.com/lumens.htm,http://fuckingclosuresyntax.com,http://devernay.free.fr/cours/opengl/materials.html,http://id3.org/,http://id3.org/d3v2.3.0,http://reactivex.io/,http://codingdojo.org/WhatIsCodingDojo/,http://xip.io/,http://codingdojo.org/kata/Minesweeper/,http://id3.org/Implementations,https://developer.android.com/studio/intro/keyboard-shortcuts.html,https://developer.android.com/studio/publish/app-signing#generate-key,https://developer.android.com/studio/publish/upload-bundle,https://www.googletagmanager.com/"  \
                         --http-status-ignore "999" \
                         --typhoeus-config "{\"followlocation\": true}" \
                         ./_site
