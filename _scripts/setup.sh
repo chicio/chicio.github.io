c0b33eb25724dc5d48b6291419d4096ede055369
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2020-05-14 00:27:39 +0200

    Updated dependecies :rocket:

--- _scripts/setup.sh
+++ _scripts/setup.sh
@@ -3,8 +3,7 @@
 # Install npm global dependecies
 npm install --global n@6.5.1
 npm install --global gulp-cli@2.2.0
-npm install --global flow-typed@2.6.2
-npm install --global flowgen@1.10.0  
+npm install --global flow-typed@3.1.0
 npm install --global @lhci/cli@0.3.x
 
 # Install gem dependecies
