cbe3a49765ba55177c5621553b86d3cc73ec3862
fduroni <fabrizio.duroni@gmail.com>
2020-10-21 09:08:16 +0200

    Fix lighthouse bug :rocket:

--- _scripts/lhci.sh
+++ _scripts/lhci.sh
@@ -1,4 +1,4 @@
 # To be used just to test locally the website with lhci
 # For CI see github action "Lighhouse" 
 
-lhci autorun --config=./.lighthouserc.json  --collect.url=https://www.fabrizioduroni.it/
+lhci autorun --config=./.lighthouserc.json  --collect.url=https://www.fabrizioduroni.it/blog/
