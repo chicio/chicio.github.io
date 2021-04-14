0548722a330091a2bcf38ca7bd5075014f3c603a
fduroni <fabrizio.duroni@gmail.com>
2021-04-14 18:05:36 +0200

    Prepare for gatsbyjs :rocket:

--- _ts/common/cookie-consent.js
+++ _ts/common/cookie-consent.js
@@ -23,3 +23,30 @@ var cookieConsent = function () {
     });
 };
 exports.cookieConsent = cookieConsent;
+
+
+if (typeof window !== "undefined") {
+    window.addEventListener('load', () => { 
+        window.cookieconsent.initialise({
+            palette: {
+                popup: {
+                    background: '#303F9F',
+                    text: '#ffffff'
+                },
+                button: {
+                    background: '#0F67FF',
+                    text: '#ffffff'
+                }
+            },
+            theme: 'classic',
+            content: {
+                dismiss: 'Ok',
+                href: window.location.protocol + '//' + window.location.host + '/cookie-policy.html',
+                message: 'This website uses cookies to ensure you get the best experience.',
+                link: 'Learn more about cookie policy'
+            }
+        });
+    });
+} else {
+    console.log("no cookieconsent");
+}
