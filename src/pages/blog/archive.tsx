a5416b5860ab07ff1cbcbbf58f3013af464aa540
fduroni <fabrizio.duroni@gmail.com>
2021-04-18 11:45:22 +0200

    New template for design system :rocket:

--- src/pages/blog/archive.tsx
+++ src/pages/blog/archive.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { graphql, PageProps } from "gatsby";
-import { ArchiveQuery } from "../../../graphql-types";
+import {
+  ArchiveQuery,
+  MarkdownRemarkFields,
+  MarkdownRemarkFrontmatter,
+  Maybe,
+} from "../../../graphql-types";
 import { track, tracking } from "../../utils/tracking";
 import { BlogPage } from "../../components/design-system/templates/blog-page";
 import { StandardInternalLink } from "../../components/design-system/atoms/standard-internal-link";
@@ -8,6 +13,7 @@ import styled from "styled-components";
 import { ContainerFluid } from "../../components/design-system/atoms/container-fluid";
 import { Time } from "../../components/design-system/atoms/time";
 import { Heading1 } from "../../components/design-system/atoms/heading1";
+import { WindowLocation } from "@reach/router";
 
 const PostContainer = styled(ContainerFluid)`
   display: flex;
@@ -41,42 +47,77 @@ const PostListTitle = styled(Heading1)`
   margin-bottom: ${(props) => props.theme.spacing[4]};
 `;
 
+interface BlogGenericPostListPageProps {
+  title: string;
+  posts: Array<{
+    node: {
+      fields?: Maybe<Pick<MarkdownRemarkFields, "slug">>;
+      frontmatter?: Maybe<Pick<MarkdownRemarkFrontmatter, "date" | "title">>;
+    };
+  }>;
+  author: string;
+  location: WindowLocation<WindowLocation["state"]>;
+  ogPageType: string;
+  ogImage: string;
+  trackingCategory: string;
+}
+
+const BlogGenericPostListPage: React.FC<BlogGenericPostListPageProps> = ({
+  title,
+  posts,
+  author,
+  location,
+  ogPageType,
+  ogImage,
+  trackingCategory,
+}) => (
+  <BlogPage
+    location={location}
+    author={author}
+    ogPageType={ogPageType}
+    ogImage={ogImage}
+    trackingCategory={trackingCategory}
+  >
+    <PostListTitle>{title}</PostListTitle>
+    {posts.map((post) => (
+      <PostContainer key={post.node.fields!.slug!}>
+        <Column size={"15%"}>
+          <PostTime>{post.node.frontmatter?.date}</PostTime>
+        </Column>
+        <Column size={"85%"}>
+          <StandardInternalLink
+            to={post.node.fields!.slug!}
+            onClick={() =>
+              track(
+                tracking.action.open_blog_post,
+                trackingCategory,
+                tracking.label.body
+              )
+            }
+          >
+            {post.node.frontmatter?.title}
+          </StandardInternalLink>
+        </Column>
+      </PostContainer>
+    ))}
+  </BlogPage>
+);
+
 const Archive: React.FC<PageProps<ArchiveQuery>> = ({ data, location }) => {
   const siteMetadata = data.site!.siteMetadata!;
   const author = siteMetadata.author!;
   const featuredImage = siteMetadata.featuredImage!;
 
   return (
-    <BlogPage
-      location={location}
+    <BlogGenericPostListPage
+      title={"Archive"}
+      posts={data.allMarkdownRemark.edges}
       author={author}
+      location={location}
       ogPageType={"website"}
       ogImage={`/${featuredImage}`}
       trackingCategory={tracking.category.blog_archive}
-    >
-      <PostListTitle>Archive</PostListTitle>
-      {data.allMarkdownRemark.edges.map((post) => (
-        <PostContainer key={post.node.fields!.slug!}>
-          <Column size={"15%"}>
-            <PostTime>{post.node.frontmatter?.date}</PostTime>
-          </Column>
-          <Column size={"85%"}>
-            <StandardInternalLink
-              to={post.node.fields!.slug!}
-              onClick={() =>
-                track(
-                  tracking.action.open_blog_post,
-                  tracking.category.blog_archive,
-                  tracking.label.body
-                )
-              }
-            >
-              {post.node.frontmatter?.title}
-            </StandardInternalLink>
-          </Column>
-        </PostContainer>
-      ))}
-    </BlogPage>
+    />
   );
 };
 
