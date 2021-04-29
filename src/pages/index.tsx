20a30b6a9fbbcc11da1d6422426f3f7af24f8332
fduroni <fabrizio.duroni@gmail.com>
2021-04-29 09:09:01 +0200

    Installed react-tsparticles :rocket:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ import { ContainerFullscreen } from "../components/design-system/atoms/container
 import { Footer } from "../components/design-system/organism/footer";
 import { tracking } from "../logic/tracking";
 import { OgPageType } from "../logic/seo";
+import Particles from "react-tsparticles";
 
 const HomePage: React.FC<PageProps> = ({ location }) => {
   const data = useStaticQuery<HomePageQuery>(
@@ -38,6 +39,88 @@ const HomePage: React.FC<PageProps> = ({ location }) => {
         imageUrl={`/${featuredImage}`}
       />
       <ContainerFullscreen>
+        <Particles
+          id="tsparticles"
+          style={{ position: "absolute", top: 0, left: 0 }}
+          options={{
+            background: {
+              color: {
+                value: "#0d47a1",
+              },
+            },
+            fpsLimit: 60,
+            interactivity: {
+              detectsOn: "canvas",
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                bubble: {
+                  distance: 400,
+                  duration: 2,
+                  opacity: 0.8,
+                  size: 40,
+                },
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#ffffff",
+              },
+              links: {
+                color: "#ffffff",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              collisions: {
+                enable: true,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outMode: "bounce",
+                random: false,
+                speed: 6,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  value_area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                random: true,
+                value: 5,
+              },
+            },
+            detectRetina: true,
+          }}
+        />
         <ProfilePresentation author={author} />
         <DownArrow />
       </ContainerFullscreen>
