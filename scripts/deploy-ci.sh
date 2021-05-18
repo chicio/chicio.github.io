e082012a55bb1ec293785e899e707ff79e471702
fduroni <fabrizio.duroni@gmail.com>
2021-05-18 10:32:18 +0200

    Centralized builg scripts :rocket:

--- scripts/deploy-ci.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-# Deploy script for CI
-
-npm install -g gatsby-cli
-npm install
-npm run deploy
--- scripts/test-ci.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-# Test script for CI
-
-npm install -g gatsby-cli
-npm install
-npm run test
