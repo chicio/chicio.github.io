3f0bfd1bf6230d030f6b01f94b4c2c4555e81238
fduroni <fabrizio.duroni@gmail.com>
2021-10-15 15:09:56 +0200

    Improved art description :sparkles:

--- src/logic/art.ts
+++ src/logic/art.ts
@@ -66,7 +66,7 @@ export const artDescriptions: { [imageName: string]: string } = {
   "2021-04-18": "Fabergé eggs 🥚🥚",
   "2021-06-06": "Iron Man 🦾🦿",
   "2021-06-22": "Ice-cream 🧊🍦",
-  "2021-07-10": "Domestika course for tattoo design",
+  "2021-07-10": "Domestika course for tattoo design process 🧪",
   "2021-07-24": "Confused bomb 💣 🥴",
   "2021-08-20": "Bumblebee 🐝",
   "2021-09-26": "Loto flower 🌸",
