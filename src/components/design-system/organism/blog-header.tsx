aa39ffabc6e9e046825f069ba8446ec0dde6e171
fduroni <fabrizio.duroni@gmail.com>
2021-10-29 12:25:06 +0200

    Niow header is responsive :rocket:

--- src/components/design-system/organism/blog-header.tsx
+++ src/components/design-system/organism/blog-header.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
 import styled from "styled-components";
-import { Heading3 } from "../atoms/heading3";
 import { mediaQuery } from "../utils-css/media-query";
 
 const BlogHeaderContainer = styled.div`
   display: flex;
   align-items: center;
-  margin-top: 50px;
-  margin-bottom: 20px;
+
+  ${mediaQuery.minWidth.sm} {
+    margin-top: ${(props) => props.theme.spacing[10]};
+    margin-bottom: ${(props) => props.theme.spacing[10]};
+  }
 `;
 
 const BlogHeaderColumn = styled.div`
@@ -17,52 +19,51 @@ const BlogHeaderColumn = styled.div`
   justify-content: center;
 `;
 
-const BlogTitle = styled(Heading3)`
+const BlogTitle = styled.span`
   color: ${(props) => props.theme.light.primaryTextColor};
   margin: 0;
   font-weight: bold;
   display: block;
   line-height: 1.5;
+  font-size: ${(props) => props.theme.fontSizes[6]};
 
   ${mediaQuery.dark} {
     color: ${(props) => props.theme.dark.primaryTextColor};
   }
 
-  ${mediaQuery.maxWidth.md} {
-    font-size: 28px;
-  }
-
-  ${mediaQuery.maxWidth.xs} {
-    font-size: 22px;
+  ${mediaQuery.minWidth.sm} {
+    font-size: ${(props) => props.theme.fontSizes[9]};
   }
 `;
 
 const BlogDescription = styled.span`
-  display: block;
-  font-size: ${(props) => props.theme.fontSizes[4]};
-  color: ${(props) => props.theme.light.secondaryTextColor};
-  line-height: 1.5;
+  display: none;
 
-  ${mediaQuery.dark} {
-    color: ${(props) => props.theme.dark.secondaryTextColor};
-  }
+  ${mediaQuery.minWidth.sm} {
+    display: block;
+    font-size: ${(props) => props.theme.fontSizes[4]};
+    color: ${(props) => props.theme.light.secondaryTextColor};
+    line-height: 1.5;
 
-  ${mediaQuery.maxWidth.md} {
-    font-size: 12px;
-  }
-
-  ${mediaQuery.maxWidth.xs} {
-    font-size: 10px;
+    ${mediaQuery.dark} {
+      color: ${(props) => props.theme.dark.secondaryTextColor};
+    }
   }
 `;
 
 const ImageContainer = styled.div`
-  width: 80px;
-  height: 80px;
+  width: 35px;
+  height: 35px;
+
   background-color: ${(props) => props.theme.light.generalBackgroundLight};
   margin-right: ${(props) => props.theme.spacing[2]};
   border-radius: 10px;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.575);
+
+  ${mediaQuery.minWidth.sm} {
+    width: 80px;
+    height: 80px;
+  }
 
   ${mediaQuery.dark} {
     background-color: ${(props) => props.theme.dark.generalBackgroundLight};
