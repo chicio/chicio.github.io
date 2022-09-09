140367a3cdae6eac224f3ea6824995f51bdb722a
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2022-09-09 15:23:30 +0200

    Updates

--- sw.js
+++ sw.js
@@ -33,11 +33,11 @@ self.__precacheManifest = [
     "url": "framework-1cc34fd1557c0d2398cf.js"
   },
   {
-    "url": "app-89d5114989040083316e.js"
+    "url": "app-d00842bb18d9895321c0.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d5dfb55f29642b8b4cb4545fd9751878"
+    "revision": "993ccc77f171995e8a28fd17035c161e"
   },
   {
     "url": "polyfill-e6afbad8eee24237725f.js"
@@ -164,7 +164,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-89d5114989040083316e.js`))) {
+  if (!resources || !(await caches.match(`/app-d00842bb18d9895321c0.js`))) {
     return await fetch(event.request)
   }
 
